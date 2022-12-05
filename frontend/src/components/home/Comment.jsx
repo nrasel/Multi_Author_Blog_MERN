84688a48483ebfc0efc03d6875b18bdd5131141c
nrasel <nrasel452@gmail.com>
2022-12-05 21:53:20 +0600

    public comments

--- frontend/src/components/home/Comment.jsx
+++ frontend/src/components/home/Comment.jsx
@@ -3,41 +3,266 @@ import { BsTrash } from "react-icons/bs";
 
 const Comment = () => {
   return (
-    <div className="comments">
-      <div className="main-reply-comment">
-        <div className="image-comment-time-name">
-          <img src="http://localhost:3000/articleImage/artificial.jpg" alt="" />
-          <div className="name-time-comment">
-            <div className="name-time">
-              <h4>Naimur Rahman</h4>
-              <span>10 day ago</span>
-            </div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry
-            </p>
-            <div className="reply-btn">
-              <button>Reply</button>
-            </div>
-            <div className="reply-box">
-              <div className="image-input">
-                <img
-                  src="http://localhost:3000/articleImage/artificial.jpg"
-                  alt=""
-                />
-                <input type="text" required placeholder="add a public reply" />
+    <>
+      <div className="comments">
+        <div className="main-reply-comment">
+          <div className="image-comment-time-name">
+            <img
+              src="http://localhost:3000/articleImage/artificial.jpg"
+              alt=""
+            />
+            <div className="name-time-comment">
+              <div className="name-time">
+                <h4>Naimur Rahman</h4>
+                <span>10 day ago</span>
               </div>
-              <div className="reply-submit">
-                
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+              <div className="reply-btn">
+                <button>Reply</button>
+              </div>
+              <div className="reply-box">
+                <div className="image-input">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <input
+                    type="text"
+                    required
+                    placeholder="add a public reply"
+                  />
+                </div>
+                <div className="reply-submit">
+                  <button className="cancel">Cancel</button>
+                  <button className="submit">Submit</button>
+                </div>
+              </div>
+              <div className="reply-comment">
+                <div className="reply-comment-image-name-time">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <div className="name-time-comment">
+                    <div className="name-time">
+                      <h4>Naimur Rahman</h4>
+                      <span>10 day ago</span>
+                    </div>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry
+                    </p>
+                    <div className="reply-btn">
+                      <button>Reply</button>
+                    </div>
+                    <div className="reply-box">
+                      <div className="image-input">
+                        <img
+                          src="http://localhost:3000/articleImage/artificial.jpg"
+                          alt=""
+                        />
+                        <input
+                          type="text"
+                          required
+                          placeholder="add a public reply"
+                        />
+                      </div>
+                      <div className="reply-submit">
+                        <button className="cancel">Cancel</button>
+                        <button className="submit">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="reply-comment">
+                <div className="reply-comment-image-name-time">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <div className="name-time-comment">
+                    <div className="name-time">
+                      <h4>Naimur Rahman</h4>
+                      <span>10 day ago</span>
+                    </div>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry
+                    </p>
+                    <div className="reply-btn">
+                      <button>Reply</button>
+                    </div>
+                    <div className="reply-box">
+                      <div className="image-input">
+                        <img
+                          src="http://localhost:3000/articleImage/artificial.jpg"
+                          alt=""
+                        />
+                        <input
+                          type="text"
+                          required
+                          placeholder="add a public reply"
+                        />
+                      </div>
+                      <div className="reply-submit">
+                        <button className="cancel">Cancel</button>
+                        <button className="submit">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="action">
+                <BsTrash />
               </div>
             </div>
           </div>
+          <div className="action">
+            <BsTrash />
+          </div>
         </div>
-        <div className="action">
-          <BsTrash />
+        <div className="main-reply-comment">
+          <div className="image-comment-time-name">
+            <img
+              src="http://localhost:3000/articleImage/artificial.jpg"
+              alt=""
+            />
+            <div className="name-time-comment">
+              <div className="name-time">
+                <h4>Naimur Rahman</h4>
+                <span>10 day ago</span>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+              </p>
+              <div className="reply-btn">
+                <button>Reply</button>
+              </div>
+              <div className="reply-box">
+                <div className="image-input">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <input
+                    type="text"
+                    required
+                    placeholder="add a public reply"
+                  />
+                </div>
+                <div className="reply-submit">
+                  <button className="cancel">Cancel</button>
+                  <button className="submit">Submit</button>
+                </div>
+              </div>
+              <div className="reply-comment">
+                <div className="reply-comment-image-name-time">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <div className="name-time-comment">
+                    <div className="name-time">
+                      <h4>Naimur Rahman</h4>
+                      <span>10 day ago</span>
+                    </div>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry
+                    </p>
+                    <div className="reply-btn">
+                      <button>Reply</button>
+                    </div>
+                    <div className="reply-box">
+                      <div className="image-input">
+                        <img
+                          src="http://localhost:3000/articleImage/artificial.jpg"
+                          alt=""
+                        />
+                        <input
+                          type="text"
+                          required
+                          placeholder="add a public reply"
+                        />
+                      </div>
+                      <div className="reply-submit">
+                        <button className="cancel">Cancel</button>
+                        <button className="submit">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="reply-comment">
+                <div className="reply-comment-image-name-time">
+                  <img
+                    src="http://localhost:3000/articleImage/artificial.jpg"
+                    alt=""
+                  />
+                  <div className="name-time-comment">
+                    <div className="name-time">
+                      <h4>Naimur Rahman</h4>
+                      <span>10 day ago</span>
+                    </div>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry
+                    </p>
+                    <div className="reply-btn">
+                      <button>Reply</button>
+                    </div>
+                    <div className="reply-box">
+                      <div className="image-input">
+                        <img
+                          src="http://localhost:3000/articleImage/artificial.jpg"
+                          alt=""
+                        />
+                        <input
+                          type="text"
+                          required
+                          placeholder="add a public reply"
+                        />
+                      </div>
+                      <div className="reply-submit">
+                        <button className="cancel">Cancel</button>
+                        <button className="submit">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="action">
+                <BsTrash />
+              </div>
+            </div>
+          </div>
+          <div className="action">
+            <BsTrash />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="comment-submit">
+        <h3>Give Your Comments</h3>
+        <div className="form-group">
+          <textarea
+            placeholder="Write something"
+            className="form-control"
+            name=""
+            id=""
+            cols="20"
+            rows="10"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <button className="btn">Submit</button>
+        </div>
+      </div>
+    </>
   );
 };
 
