f181dacdfd5e0e2e5e32e65bd4e720dbd9efa8e3
nrasel <nrasel452@gmail.com>
2022-12-07 01:20:28 +0600

    require jodit editor

--- frontend/src/components/dashboard/ArticleAdd.jsx
+++ frontend/src/components/dashboard/ArticleAdd.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { BsCardImage } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const ArticleAdd = () => {
@@ -11,7 +12,9 @@ const ArticleAdd = () => {
       <div className="add">
         <div className="title-show-article">
           <h2>Add Article</h2>
-          <Link to="/dashboard/all-article">All Article</Link>
+          <Link className="btn" to="/dashboard/all-article">
+            All Article
+          </Link>
         </div>
         <form>
           <div className="form-group">
@@ -39,7 +42,8 @@ const ArticleAdd = () => {
           <button className="btn">Update</button>
           <div className="form-group">
             <label htmlFor="categoryadd">Category</label>
-            <select name="category" id="categoryadd">
+            <select className="form-control" name="category" id="categoryadd">
+              <option value="autoselect">--Select Article Category--</option>
               <option value="cat1">Programming</option>
               <option value="cat2">Algorithm</option>
               <option value="cat3">Computer</option>
@@ -49,13 +53,27 @@ const ArticleAdd = () => {
           </div>
           <div className="form-group">
             <label htmlFor="tags">Category</label>
-            <select name="tags" id="tags">
+            <select className="form-control" name="tags" id="tags">
+              <option value="autoselect">--Select Article Tag--</option>
               <option value="cat1">Programming</option>
               <option value="cat2">Algorithm</option>
               <option value="cat3">Computer</option>
               <option value="cat4">Electrical</option>
             </select>
             <p>Please Provide article slug</p>
+          </div>
+          <div className="form-group img-upload">
+            <div className="upload">
+              <label htmlFor="uploadImage">
+                <BsCardImage />
+              </label>
+              <input type="file" id="uploadImage" />
+            </div>
+            <label htmlFor="articleText">Article Text</label>
+            <joditEditor>
+              value={"hello"}
+              tabIndex={1}
+            </joditEditor>
           </div>
         </form>
       </div>
