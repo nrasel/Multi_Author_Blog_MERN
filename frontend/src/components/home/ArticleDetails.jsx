e1a67b9dd644b14fa35da1f8b0305e5aa6cc7bc5
nrasel <nrasel452@gmail.com>
2022-12-28 23:41:41 +0600

    article info get redux and show it article details page

--- frontend/src/components/home/ArticleDetails.jsx
+++ frontend/src/components/home/ArticleDetails.jsx
@@ -1,8 +1,10 @@
+import moment from "moment";
 import React, { useEffect } from "react";
+import htmlToText from "react-html-parser";
 import { AiFillDislike, AiFillLike, AiFillTag } from "react-icons/ai";
 import { BsChevronRight } from "react-icons/bs";
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { get_article_details } from "../../store/actions/home/articleReadAction";
 import Comment from "./Comment";
@@ -10,6 +12,9 @@ import Comment from "./Comment";
 const ArticleDetails = () => {
   const { slug } = useParams();
   const dispatch = useDispatch();
+  const { readMore, relatedArticle, moreTag, readArticle } = useSelector(
+    (state) => state.homeReducer
+  );
   useEffect(() => {
     dispatch(get_article_details(slug));
   }, [dispatch, slug]);
@@ -20,15 +25,17 @@ const ArticleDetails = () => {
         <span className="arrow">
           <BsChevronRight />
         </span>
-        <Link>Algorithm</Link>
+        <Link to={`/article/category/${readArticle?.category_slug}`}>
+          {readArticle?.category}
+        </Link>
         <span className="arrow">
           <BsChevronRight />
         </span>
-        <span>Lorem Ipsum is simply dummy text of the printing</span>
+        <span>{readArticle?.title}</span>
       </div>
       <div className="title">
         <h3>
-          <Link>Lorem Ipsum is simply dummy text of the printing</Link>
+          <Link to="#">{readArticle?.title}</Link>
         </h3>
       </div>
       <div className="auth-time">
@@ -36,30 +43,25 @@ const ArticleDetails = () => {
           <h4>
             <AiFillTag />
             <span>
-              <Link>Programming</Link>
+              <Link to={`/article/tag/${readArticle?.tag_slug}`}>
+                {readArticle?.tag}
+              </Link>
             </span>
           </h4>
         </div>
         <div className="time">
-          <span>2 jun 2022</span>
+          <span>{moment(readArticle?.updatedAt).fromNow()}</span>
         </div>
       </div>
       <div className="home-article-image">
-        <img src="http://localhost:3000/articleImage/artificial.jpg" alt="" />
+        <img
+          src={`http://localhost:3000/articleImage/${readArticle?.image}`}
+          alt=""
+        />
       </div>
       <div className="home-article-text">
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-        <p>
+        <p>{htmlToText(readArticle?.article_text)}</p>
+        <p style={{ marginTop: "18px" }}>
           <img src="http://localhost:3000/articleImage/artificial.jpg" alt="" />
         </p>
       </div>
@@ -91,13 +93,17 @@ const ArticleDetails = () => {
       </div>
       <div className="read-more">
         <span>Read More : </span>
-        <Link to="/">Lorem Ipsum is simply dummy text of the printing</Link>
+        <Link to={readMore?.slug}>{readMore?.title}</Link>
       </div>
       <div className="more-tags">
         <span>Tags</span>
-        <Link>Computer</Link>
-        <Link>Programming</Link>
-        <Link>Java</Link>
+        {moreTag.length > 0
+          ? moreTag.map((t, index) => (
+              <Link to={`/article/tag/${t}`} key={index}>
+                {t.split("-").join(" ")}
+              </Link>
+            ))
+          : ""}
       </div>
       <div className="social-icon">
         <a className="l1" href="/">
@@ -118,27 +124,19 @@ const ArticleDetails = () => {
           <h3>Related Articles</h3>
         </div>
         <div className="articles">
-          <Link className="article" to="/">
-            <img
-              src="http://localhost:3000/articleImage/artificial.jpg"
-              alt=""
-            />
-            <span>Lorem Ipsum is simply dummy text of the printing</span>
-          </Link>
-          <Link className="article" to="/">
-            <img
-              src="http://localhost:3000/articleImage/artificial.jpg"
-              alt=""
-            />
-            <span>Lorem Ipsum is simply dummy text of the printing</span>
-          </Link>
-          <Link className="article" to="/">
-            <img
-              src="http://localhost:3000/articleImage/artificial.jpg"
-              alt=""
-            />
-            <span>Lorem Ipsum is simply dummy text of the printing</span>
-          </Link>
+          {relatedArticle.length > 0 ? (
+            relatedArticle.map((art, index) => (
+              <Link to={`/article/details/${art.slug}`} className="article">
+                <img
+                  src={`http://localhost:3000/articleImage/${art.image}`}
+                  alt=""
+                />
+                <span>{htmlToText(art.article_text.slice(0, 90))}</span>
+              </Link>
+            ))
+          ) : (
+            <span>No related article</span>
+          )}
         </div>
       </div>
       <div className="comment-title">
